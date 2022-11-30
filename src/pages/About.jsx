679f1b5bc10acf763b099dd536f34cdfc09e4b69
Ivan Vasylynyuk <>
2022-11-30 03:15:50 +0000

    Initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Tilde gastropub flannel, cliche la croix seitan tumeric unicorn cronut. Post-ironic gentrify small batch distillery fingerstache forage same mixtape tbh. Praxis stumptown gluten-free godard small batch DSA four loko authentic palo santo. You probably haven't heard of them four dollar toast PBR&B cred pork belly organic microdosing art party raclette distillery put a bird on it mixtape vegan. Palo santo waistcoat literally mustache hoodie, poke air plant banjo distillery.</p>
+    </section>
+  )
+}
+
+export default About
