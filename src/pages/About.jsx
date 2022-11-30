47a00aa52f92688fb96ca60ae3e67155b3162c1a
jvin08 <jvin08@gmail.com>
2022-11-29 21:37:31 -0600

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
-      <p>Tilde gastropub flannel, cliche la croix seitan tumeric unicorn cronut. Post-ironic gentrify small batch distillery fingerstache forage same mixtape tbh. Praxis stumptown gluten-free godard small batch DSA four loko authentic palo santo. You probably haven't heard of them four dollar toast PBR&B cred pork belly organic microdosing art party raclette distillery put a bird on it mixtape vegan. Palo santo waistcoat literally mustache hoodie, poke air plant banjo distillery.</p>
+      <p>Tilde$ gastropub flannel, cliche la croix seitan tumeric unicorn cronut. Post-ironic gentrify small batch distillery fingerstache forage same mixtape tbh. Praxis stumptown gluten-free godard small batch DSA four loko authentic palo santo. You probably haven't heard of them four dollar toast PBR&B cred pork belly organic microdosing art party raclette distillery put a bird on it mixtape vegan. Palo santo waistcoat literally mustache hoodie, poke air plant banjo distillery.</p>
     </section>
   )
 }
